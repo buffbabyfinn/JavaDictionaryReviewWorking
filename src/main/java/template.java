bda0db1bc506a93995879ed17df6fd720cab6161
Megan <mefayer@gmail.com>
2016-02-08 20:20:58 -0800

    add ful vtl links and pages

--- src/main/java/template.java
+++ src/main/java/template.java
@@ -8,12 +8,23 @@ import static spark.Spark.*;
 public class template {
   public static void main( String[] args ) {
     staticFileLocation("/public");
-
+    String layout = "templates/layout.vtl";
     // System.out.println( "Hello World!" );
 
     get("/", (request, response) -> {
-      return new ModelAndView(new HashMap(), "templates/hello.vtl");
-    }, new VelocityTemplateEngine());
+        HashMap model = new HashMap();
+        model.put("template", "templates/hello.vtl" );
+
+        return new ModelAndView(model, "templates/layout.vtl");
+      }, new VelocityTemplateEngine());
+    };
+
+    get("/other", (request, response) -> {
+        HashMap model = new HashMap();
+        model.put("template", "templates/other.vtl" );
+
+        return new ModelAndView(model, "templates/layout.vtl");
+      }, new VelocityTemplateEngine());
     };
   }
 }
