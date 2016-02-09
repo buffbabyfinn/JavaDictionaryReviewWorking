3433a3b008c5e2503e7874c043b8a4114dfead2b
Megan <mefayer@gmail.com>
2016-02-08 20:09:11 -0800

    add vtl template and style file

--- src/main/java/template.java
+++ src/main/java/template.java
@@ -1,5 +1,19 @@
+import java.util.HashMap;
+
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import static spark.Spark.*;
+
 public class template {
   public static void main( String[] args ) {
-    System.out.println( "Hello World!" );
+    staticFileLocation("/public");
+
+    // System.out.println( "Hello World!" );
+
+    get("/", (request, response) -> {
+      return new ModelAndView(new HashMap(), "templates/hello.vtl");
+    }, new VelocityTemplateEngine());
+    };
   }
 }
